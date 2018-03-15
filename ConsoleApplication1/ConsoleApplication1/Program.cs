338173d1aaac1b11a559e94a6708786cab375118
m305-27\w401 <albert0807@gmail.com>
2018-03-15 18:01:20 +0800

    y04

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("體重:");
+            string weight =Console.ReadLine();
+            Console.WriteLine("身高");
+            string height = Console.ReadLine();
+            double BMI;
+            BMI = double.Parse(weight) / double.Parse(height) / double.Parse(height) * 10000;
+            if (BMI > 31.5)
+                Console.WriteLine("你免役");
+            else
+                Console.WriteLine("去當兵");
+            Console.WriteLine("你的BMI是:{0}",BMI);
+            Console.ReadLine();
+        }
+    }
+}
